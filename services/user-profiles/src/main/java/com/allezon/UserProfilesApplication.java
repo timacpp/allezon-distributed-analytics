bbee5824ec404bcbafe5fc28b63b15561f8a11f7
Tima <timathevedmed@gmail.com>
2023-07-29 13:17:59 +0200

    Migrate to microservice architecture

---
+++ services/user-profiles/src/main/java/com/allezon/UserProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.allezon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfilesApplication.class, args);
+	}
+
+}
