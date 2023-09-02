ace1c859b44937e8bece82e9b996e8b18fc2c723
Tima <timathevedmed@gmail.com>
2023-09-02 15:05:46 +0200

    Add static parse method to TimeRange

--- common-core/src/main/java/com/allezon/core/domain/TimeRange.java
+++ common-core/src/main/java/com/allezon/core/domain/TimeRange.java
@@ -6,12 +6,13 @@ public record TimeRange(Instant start, Instant end) {
 	public static final TimeRange ANY = new TimeRange(Instant.MIN, Instant.MAX);
 	private static final String UTC_SUFFIX = "Z";
 
-	public TimeRange(String timeRange) {
-		this(timeRange.split("_"));
+	public static TimeRange parse(String timeRange) {
+		String[] range = timeRange.split("_");
+		return new TimeRange(range[0] + UTC_SUFFIX, range[1] + UTC_SUFFIX);
 	}
 
-	private TimeRange(String[] range) {
-		this(Instant.parse(range[0] + UTC_SUFFIX), Instant.parse(range[1] + UTC_SUFFIX));
+	private TimeRange(String start, String end) {
+		this(Instant.parse(start), Instant.parse(end));
 	}
 
 	public boolean includes(Instant instant) {
