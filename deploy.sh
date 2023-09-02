d747f4b51782fc78b92620d1b94b74ff37ccad6b
Tima <timathevedmed@gmail.com>
2023-09-02 14:55:58 +0200

    Add option to deploy a single service

--- deploy.sh
+++ deploy.sh
@@ -10,18 +10,24 @@ function deploy {
   docker build -t "$image" "$1"
   docker tag "$image" "$tag"
   docker push "$DOCKER_REGISTRY"/"$image"
-  for host in "${@:2}"; do
-    sshpass -p "$PASSWORD" ssh "$USER@$USER$host.rtb-lab.pl" "
+  for vm in "${@:2}"; do
+    sshpass -p "$PASSWORD" ssh "st101@st101$vm.rtb-lab.pl" "
         docker pull $tag &&
         docker rm -f $container &&
         docker run -d --net=host --name $container $image"
   done
 }
 
-mvn --file parent-project clean install
-docker run -d -p 5000:5000 --name registry registry:2 2> /dev/null
-deploy haproxy vm102
-deploy aerospike vm103 vm104
-deploy user-tags-api vm108 # vm105 vm106
-deploy user-profiles-api vm109 # vm107 vm108
-deploy aggregates-api vm110 # vm109 vm110
+if [[ -n $1 && -n $2 ]]; then
+  if [[ $1 == *-api ]]; then
+    mvn --file parent-project clean install
+  fi
+  deploy "$1" "$2"
+else
+  mvn --file parent-project clean install
+  deploy haproxy vm102
+  deploy aerospike vm103 vm104
+  deploy user-tags-api vm108
+  deploy user-profiles-api vm109
+  deploy aggregates-api vm110
+fi
