a80bd83f32b055ee337ecf3af4b331e74d435d19
Tima <timathevedmed@gmail.com>
2023-09-02 15:40:51 +0200

    Source sdkman after installation

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ set -eo pipefail
 
 sudo apt -y install ansible sshpass
 curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install maven
 sdk install java 20-temurin
 
