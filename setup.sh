1f4d6eebd3e53a193f72375e4d772ead16ada4fe
Tima <timathevedmed@gmail.com>
2023-09-03 13:25:19 +0200

    Remove kafka from setup script

--- setup.sh
+++ setup.sh
@@ -3,13 +3,6 @@
 source .env
 set -eo pipefail
 
-function setup {
-  cd "$1"
-  ansible-playbook -i hosts --extra-vars \
-     "ansible_user=st101 ansible_password=$PASSWORD ansible_ssh_extra_args='-o StrictHostKeyChecking=no'" playbook.yaml
-  cd ..
-}
-
 if [[ -z $(which java) ]]; then
   sudo apt -y install ansible sshpass zip
   curl -s "https://get.sdkman.io" | bash
@@ -18,9 +11,7 @@ if [[ -z $(which java) ]]; then
   sdk install java 20-tem
 fi
 
-if [[ -n $1 ]]; then
-  setup "$1"
-else
-  setup docker-registry
-  setup kafka
-fi
+cd "$1"
+ansible-playbook -i hosts --extra-vars \
+  "ansible_user=st101 ansible_password=$PASSWORD ansible_ssh_extra_args='-o StrictHostKeyChecking=no'" playbook.yaml
+cd ..
