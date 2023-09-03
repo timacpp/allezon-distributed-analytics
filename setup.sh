0ebf0d9989c9e945043f2c245e4ed59b542843dc
Tima <timathevedmed@gmail.com>
2023-09-03 16:44:01 +0200

    Add Kafka service

--- setup.sh
+++ setup.sh
@@ -3,6 +3,18 @@
 source .env
 set -eo pipefail
 
+function setup {
+  cd "$1"
+  ansible-playbook -i hosts --extra-vars \
+    "ansible_user=st101 ansible_password=$PASSWORD ansible_ssh_extra_args='-o StrictHostKeyChecking=no'" playbook.yaml
+  cd ..
+}
+
+if [[ -n $1 ]]; then
+  setup "$1"
+  exit
+fi
+
 if [[ -z $(which java) ]]; then
   sudo apt -y install ansible sshpass zip
   curl -s "https://get.sdkman.io" | bash
@@ -11,7 +23,5 @@ if [[ -z $(which java) ]]; then
   sdk install java 20-tem
 fi
 
-cd "$1"
-ansible-playbook -i hosts --extra-vars \
-  "ansible_user=st101 ansible_password=$PASSWORD ansible_ssh_extra_args='-o StrictHostKeyChecking=no'" playbook.yaml
-cd ..
+setup docker-registry
+setup kafka
