2d53b5d5fa6a11ca089d0d3c11de4ebbbdff0fb2
tvedmedenko <tymofii.vedmedenko@rtbhouse.com>
2023-09-06 19:48:49 +0200

    Change logger info to debug

--- user-tags-api/src/main/java/com/allezon/tags/UserTagEventPublisher.java
+++ user-tags-api/src/main/java/com/allezon/tags/UserTagEventPublisher.java
@@ -1,6 +1,5 @@
 package com.allezon.tags;
 
-import com.allezon.core.constants.KafkaConstants;
 import com.allezon.core.domain.UserTag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +15,7 @@ public class UserTagEventPublisher {
 	private KafkaTemplate<String, UserTag> kafkaTemplate;
 
 	public void publish(UserTag userTag) {
-		logger.info("Publishing user tag: " + userTag);
+		logger.debug("Publishing user tag: " + userTag);
 		kafkaTemplate.send("user-tags", userTag);
 	}
 }
