b1704c2fc1c374114f0a6b86518798d20799dccf
tvedmedenko <tymofii.vedmedenko@rtbhouse.com>
2023-09-07 14:50:17 +0200

    Set user-tags topic partitions to 3

--- user-tags-api/src/main/java/com/allezon/tags/UserTagApplication.java
+++ user-tags-api/src/main/java/com/allezon/tags/UserTagApplication.java
@@ -1,13 +1,24 @@
 package com.allezon.tags;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
 
 @EnableKafka
 @SpringBootApplication
 public class UserTagApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserTagApplication.class, args);
+	}
+
+	@Bean
+	public NewTopic newTopic() {
+		return TopicBuilder.name("user-tags")
+				.partitions(3)
+				.replicas(2)
+				.build();
 	}
 }
