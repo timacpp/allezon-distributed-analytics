21dcff3eaf25524c7eb041548b242f00297ad959
tvedmedenko <tymofii.vedmedenko@rtbhouse.com>
2023-09-10 20:39:11 +0200

    Structure setup script

--- setup.sh
+++ setup.sh
@@ -10,22 +10,13 @@ function setup {
   cd ..
 }
 
-if [[ -n $1 ]]; then
-  for project in "$@"; do
-    setup $project
+function setupNetwork {
+  for i in $(seq -w 01 10); do
+    sshpass -p "$PASSWORD" ssh st101@st101vm1"$i".rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true";
   done
-  exit
-fi
+}
 
-for i in $(seq -w 01 10); do
-  sshpass -p "$PASSWORD" ssh st101@st101vm1"$i".rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true";
-done
-
-setup docker-registry
-setup kafka
-setup aerospike
-
-if [[ -z $(which java) ]]; then
+function setupJava {
   sudo add-apt-repository -y ppa:ansible/ansible
   sudo apt -y install ansible sshpass zip
   curl -s "https://get.sdkman.io" | bash
@@ -33,4 +24,17 @@ if [[ -z $(which java) ]]; then
   sdk install maven
   sdk install java 20-tem
   source "$HOME/.sdkman/bin/sdkman-init.sh"
+}
+
+if [[ -n $1 ]]; then
+  for project in "$@"; do
+    setup $project
+  done
+  exit
 fi
+
+setupNetwork
+setup docker-registry
+setup kafka
+setup aerospike
+setupJava
