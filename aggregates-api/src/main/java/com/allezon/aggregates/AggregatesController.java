847c4e7383fb6e314ea8e07a288377b67e944ed3
tvedmedenko <tymofii.vedmedenko@rtbhouse.com>
2023-09-10 12:39:12 +0200

    Fix indents in AggregatesController

--- aggregates-api/src/main/java/com/allezon/aggregates/AggregatesController.java
+++ aggregates-api/src/main/java/com/allezon/aggregates/AggregatesController.java
@@ -17,28 +17,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/aggregates")
 public class AggregatesController {
-	private static final Logger logger = LoggerFactory.getLogger(AggregatesController.class);
+    private static final Logger logger = LoggerFactory.getLogger(AggregatesController.class);
 
-	@Autowired
-	private AggregatesService aggregatesService;
+    @Autowired
+    private AggregatesService aggregatesService;
 
-	@PostMapping("/aggregates")
-	public ResponseEntity<AggregatesWindow> getAggregates(
-			@RequestParam("time_range") String timeRange,
-			@RequestParam("action") UserTag.Action action,
-			@RequestParam("aggregates") List<AggregationOperator> operators,
-			@RequestParam(value = "origin", required = false) String origin,
-			@RequestParam(value = "brand_id", required = false) String brandId,
-			@RequestParam(value = "category_id", required = false) String categoryId,
-			@RequestBody(required = false) AggregatesWindow expectedAggregates) {
-		QueryFilter filter = new QueryFilter(origin, brandId, categoryId);
-		AggregatesWindow aggregates = aggregatesService.query(TimeRange.parse(timeRange), action, operators, filter);
+    @PostMapping("/aggregates")
+    public ResponseEntity<AggregatesWindow> getAggregates(
+            @RequestParam("time_range") String timeRange,
+            @RequestParam("action") UserTag.Action action,
+            @RequestParam("aggregates") List<AggregationOperator> operators,
+            @RequestParam(value = "origin", required = false) String origin,
+            @RequestParam(value = "brand_id", required = false) String brandId,
+            @RequestParam(value = "category_id", required = false) String categoryId,
+            @RequestBody(required = false) AggregatesWindow expectedAggregates) {
+        QueryFilter filter = new QueryFilter(origin, brandId, categoryId);
+        AggregatesWindow aggregates = aggregatesService.query(TimeRange.parse(timeRange), action, operators, filter);
 
-		if (!aggregates.equals(expectedAggregates)) {
+        if (!aggregates.equals(expectedAggregates)) {
             logger.error("Aggregates mismatch for timeRange={}, actual={}, expected={}",
                     timeRange, aggregates, expectedAggregates);
-		}
+        }
 
-		return ResponseEntity.ok(aggregates);
-	}
+        return ResponseEntity.ok(aggregates);
+    }
 }
