020410fff7c43479bccd3b04f1cd144a2dd96707
tvedmedenko <tymofii.vedmedenko@rtbhouse.com>
2023-09-11 11:34:31 +0200

    Setup dependencies before network

--- setup.sh
+++ setup.sh
@@ -3,27 +3,26 @@
 source .env
 set -eo pipefail
 
-function setup {
+function setupProject {
   cd "$1"
   ansible-playbook -i ../hosts --extra-vars \
     "ansible_user=st101 ansible_password=$PASSWORD ansible_ssh_extra_args='-o StrictHostKeyChecking=no'" playbook.yaml
   cd ..
 }
 
-function setupNetwork {
-  for i in $(seq -w 01 10); do
-    sshpass -p "$PASSWORD" ssh st101@st101vm1"$i".rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true";
-  done
-}
-
-function setupJava {
+function setupDependencies {
   sudo add-apt-repository -y ppa:ansible/ansible
   sudo apt -y install ansible sshpass zip
   curl -s "https://get.sdkman.io" | bash
   source "$HOME/.sdkman/bin/sdkman-init.sh"
   sdk install maven
   sdk install java 20-tem
-  source "$HOME/.sdkman/bin/sdkman-init.sh"
+}
+
+function setupNetwork {
+  for i in $(seq -w 01 10); do
+    sshpass -p "$PASSWORD" ssh st101@st101vm1"$i".rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true";
+  done
 }
 
 if [[ -n $1 ]]; then
@@ -33,8 +32,9 @@ if [[ -n $1 ]]; then
   exit
 fi
 
+setupDependencies
 setupNetwork
-setup docker-registry
-setup kafka
-setup aerospike
-setupJava
+setupProject docker-registry
+setupProject kafka
+setupProject aerospike
+source "$HOME/.sdkman/bin/sdkman-init.sh"
