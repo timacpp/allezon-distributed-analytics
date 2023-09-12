fda87ac728dbfe6d41a337dc068578e94c0dea57
tvedmedenko <tymofii.vedmedenko@rtbhouse.com>
2023-09-12 12:40:43 +0200

    Remove serializable from Aggregate

--- common-core/src/main/java/com/allezon/core/domain/aggregate/Aggregate.java
+++ common-core/src/main/java/com/allezon/core/domain/aggregate/Aggregate.java
@@ -1,6 +1,4 @@
 package com.allezon.core.domain.aggregate;
 
-import java.io.Serializable;
-
-public record Aggregate(String hash, long sum, long count) implements Serializable {
+public record Aggregate(String hash, long sum, long count)  {
 }
